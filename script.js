5411497f18371e781488e40287e09ac66c53be86
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-18 18:35:16 -0500

    Add event listener for start button click

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-console.log("Soap Quiz Loaded!");
+document.getElementById("startButton").addEventListener("click", function(){
+
+    alert("Welcome! The quiz will begin in the next step.");
+
+});
