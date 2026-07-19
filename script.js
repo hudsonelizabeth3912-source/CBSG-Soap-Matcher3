a53a56bbf72f0f45a9d8857e1df33b26672c0a56
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-18 21:49:19 -0500

    Rename findSoap to startQuiz and update function

--- script.js
+++ script.js
@@ -1,51 +1,5 @@
-function findSoap() {
+function startQuiz() {
 
-
-    let skin = document.getElementById("skin").value;
-
-    let scent = document.getElementById("scent").value;
-
-
-    let result;
-
-
-    if (skin === "sensitive") {
-
-        result = "We recommend our gentle oatmeal soap for sensitive skin!";
-
-    } 
-    
-    else if (skin === "dry") {
-
-        result = "We recommend our moisturizing handmade soap!";
-
-    } 
-    
-    else if (scent === "floral") {
-
-        result = "We recommend our relaxing lavender floral soap!";
-
-    } 
-    
-    else if (scent === "fresh") {
-
-        result = "We recommend our refreshing citrus soap!";
-
-    } 
-    
-    else if (scent === "earthy") {
-
-        result = "We recommend our natural earthy soap!";
-
-    } 
-    
-    else {
-
-        result = "We recommend trying one of our handmade soaps!";
-
-    }
-
-
-    document.getElementById("result").innerHTML = result;
+    alert("Welcome to the Coastal Bend Soap Finder!");
 
 }
