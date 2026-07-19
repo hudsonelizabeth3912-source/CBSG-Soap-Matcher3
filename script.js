8be3f3d83e2b995158362067562b0f4b92bee432
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-19 18:12:06 -0500

    Simplify quiz start function and score handling
    Refactor quiz start logic and event listener setup.

--- script.js
+++ script.js
@@ -1,56 +1,11 @@
-// ----------------------
-// Start the quiz
-// ----------------------
+document.addEventListener("DOMContentLoaded", function () {
 
-function startQuiz() {
-    window.location.href = "question1.html";
-}
+    const startButton = document.getElementById("startButton");
 
-
-// ----------------------
-// Hidden score system
-// ----------------------
-
-let scores = {
-
-    perfectMan: 0,
-    turtleBeach: 0,
-    coffeeShop: 0,
-    lavender: 0,
-    shellSeeker: 0,
-    oatmealMilkHoney: 0,
-    bourbonVanilla: 0,
-    crazyForCoconut: 0
-
-};
-
-
-// ----------------------
-// Question 1
-// ----------------------
-
-function answerGender(gender) {
-
-    if (gender === "male") {
-
-        scores.perfectMan++;
-        scores.turtleBeach++;
-        scores.coffeeShop++;
-        scores.bourbonVanilla++;
-
+    if (startButton) {
+        startButton.addEventListener("click", function () {
+            window.location.href = "question1.html";
+        });
     }
 
-    else if (gender === "female") {
-
-        scores.lavender++;
-        scores.shellSeeker++;
-        scores.oatmealMilkHoney++;
-        scores.crazyForCoconut++;
-
-    }
-
-    console.log(scores);
-
-    alert("Question 1 Complete!");
-
-}
+});
