1bcf6ab6d94698916628a6eafb9b9bfd66565011
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-18 21:43:45 -0500

    Enhance soap recommendations for user inputs

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 function findSoap() {
 
+
     let skin = document.getElementById("skin").value;
+
     let scent = document.getElementById("scent").value;
 
-    let result = "";
+
+    let result;
+
 
     if (skin === "sensitive") {
-        result = "Try our gentle oatmeal soap!";
+
+        result = "We recommend our gentle oatmeal soap for sensitive skin!";
+
     } 
+    
+    else if (skin === "dry") {
+
+        result = "We recommend our moisturizing handmade soap!";
+
+    } 
+    
     else if (scent === "floral") {
-        result = "Try our relaxing lavender soap!";
+
+        result = "We recommend our relaxing lavender floral soap!";
+
     } 
+    
     else if (scent === "fresh") {
-        result = "Try our refreshing citrus soap!";
+
+        result = "We recommend our refreshing citrus soap!";
+
     } 
+    
+    else if (scent === "earthy") {
+
+        result = "We recommend our natural earthy soap!";
+
+    } 
+    
     else {
-        result = "Try one of our handmade natural soaps!";
+
+        result = "We recommend trying one of our handmade soaps!";
+
     }
 
+
     document.getElementById("result").innerHTML = result;
+
 }
