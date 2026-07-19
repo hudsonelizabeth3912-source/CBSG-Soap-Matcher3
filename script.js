dc2a2c7a519ac4ad8dde2048d592ed3a6df68a3d
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-19 17:56:08 -0500

    Implement hidden score system for quiz
    Added a hidden score system to track quiz results based on user responses.

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
+// ----------------------
+// Start the quiz
+// ----------------------
+
 function startQuiz() {
     window.location.href = "quiz.html";
 }
 
 
-function choosePersonality(personality) {
+// ----------------------
+// Hidden score system
+// ----------------------
 
-    if (personality === "Relaxing") {
-        alert("Your perfect match is Lavender Soap!");
+let scores = {
+
+    perfectMan: 0,
+    turtleBeach: 0,
+    coffeeShop: 0,
+    lavender: 0,
+    shellSeeker: 0,
+    oatmealMilkHoney: 0,
+    bourbonVanilla: 0,
+    crazyForCoconut: 0
+
+};
+
+
+// ----------------------
+// Question 1
+// ----------------------
+
+function answerGender(gender) {
+
+    if (gender === "male") {
+
+        scores.perfectMan++;
+        scores.turtleBeach++;
+        scores.coffeeShop++;
+        scores.bourbonVanilla++;
+
     }
 
-    else if (personality === "Energetic") {
-        alert("Your perfect match is Citrus Soap!");
+    else if (gender === "female") {
+
+        scores.lavender++;
+        scores.shellSeeker++;
+        scores.oatmealMilkHoney++;
+        scores.crazyForCoconut++;
+
     }
 
-    else if (personality === "Natural") {
-        alert("Your perfect match is Oatmeal Soap!");
-    }
+    console.log(scores);
+
+    alert("Question 1 Complete!");
 
 }
