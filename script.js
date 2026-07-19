6bee2878316eba3504b5773105844e652637fa8a
hudsonelizabeth3912-source <hudson.elizabeth3912@gmail.com>
2026-07-18 22:16:03 -0500

    Add choosePersonality function for quiz results

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function startQuiz() {
     window.location.href = "quiz.html";
 }
+
+
+function choosePersonality(personality) {
+
+    if (personality === "Relaxing") {
+        alert("Your perfect match is Lavender Soap!");
+    }
+
+    else if (personality === "Energetic") {
+        alert("Your perfect match is Citrus Soap!");
+    }
+
+    else if (personality === "Natural") {
+        alert("Your perfect match is Oatmeal Soap!");
+    }
+
+}
